2bdfc3e5d42dad8a7a03cfa42401fc0d24b54def
daniellee70 <104069507+daniellee70@users.noreply.github.com>
2022-07-28 09:33:35 -0400

    Added the plot_location_qoutient file

---
+++ 03_visualize_data/plot_location_quotient.R
@@ -0,0 +1,357 @@
+# Plot LQ ----
+
+# Set up
+
+# Core
+library(tidyverse)
+library(tidyquant)
+
+# Time series
+library(timetk)
+
+# Interactive visualization
+library(plotly)
+library(tidytext)
+
+
+# Import data ----
+
+gdp_LQ_tbl <- read_rds("00_data/gdp_LQ_tbl.rds")
+
+# Plot ----
+
+## Heatmap: all years ----
+# Compare NH and MA
+
+### * By location quotient ----
+
+g <- gdp_LQ_tbl %>%
+
+    filter(place %in% c("New Hampshire", "Massachusetts")) %>%
+
+    # Plot
+    ggplot(aes(year %>% as.factor(),
+               NAICS_desc %>%
+                   str_trunc(20, side = "right") %>%
+                   reorder_within(-LQ_rank, place),
+               text = label_lq)) +
+    geom_tile(aes(fill = -LQ_rank)) +
+    facet_wrap(~place, scales = "free_y") +
+
+    # Formatting
+    scale_fill_gradient2(low = palette_light()[1],
+                         mid = "white",
+                         high = palette_light()[2],
+                         midpoint = -10) +
+    scale_y_reordered() +
+    theme_tq() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none",
+        plot.title = element_text(face = "bold"),
+        plot.caption = element_text(face = "bold.italic")
+    ) +
+
+    labs(
+        title = "Which industries are major drivers of the economy?",
+        subtitle = "measured in location quotient",
+        caption = "New Hampshire's top industry is Retail while...",
+        x     = "",
+        y     = ""
+    )
+
+
+g %>% ggplotly(tooltip = "text")
+
+
+### * By location quotient in dollars ----
+
+g <- gdp_LQ_tbl %>%
+
+    filter(place %in% c("New Hampshire", "Massachusetts")) %>%
+
+    # Plot
+    ggplot(aes(year %>% as.factor(),
+               NAICS_desc %>%
+                   str_trunc(20, side = "right") %>%
+                   reorder_within(-LQ_dollar_rank, place),
+               text = label_LQ_dollar)) +
+    geom_tile(aes(fill = -LQ_dollar_rank)) +
+    facet_wrap(~place, scales = "free_y") +
+
+    # Formatting
+    scale_fill_gradient2(low = palette_light()[1],
+                         mid = "white",
+                         high = palette_light()[2],
+                         midpoint = -10) +
+    scale_y_reordered() +
+    theme_tq() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none",
+        plot.title = element_text(face = "bold"),
+        plot.caption = element_text(face = "bold.italic")
+    ) +
+
+    labs(
+        title = "Which industries are major drivers of the economy?",
+        subtitle = "measured in location quotient $",
+        caption = "New Hampshire's top industry is Retail while...",
+        x     = "",
+        y     = ""
+    )
+
+
+g %>% ggplotly(tooltip = "text")
+
+## Heatmap: all 50 states ----
+# Compare 1997 and 2021
+
+## * By location quotient ----
+
+g <- gdp_LQ_tbl %>%
+
+    filter(year %in% c(min(year), max(year))) %>%
+
+    ggplot(aes(x = NAICS_desc %>% str_trunc(20, side = c("right")),
+               y = place,
+               text = label_lq)) +
+    geom_tile(aes(fill = log(LQ))) +
+
+    facet_wrap(~year, scales = "free") +
+
+    scale_fill_gradient2(low = palette_light()[5],
+                         mid = "white",
+                         high = palette_light()[2],
+                         midpoint = 0) +
+    labs(
+        title = "Location Quotient",
+        y     = "",
+        x     = "NAICS Sectors - 2 digits"
+    ) +
+
+    theme_tq() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none",
+        plot.title = element_text(face = "bold"),
+        plot.caption = element_text(face = "bold.italic")
+    )
+
+g %>% ggplotly(tooltip = "text")
+
+
+## * By location quotient in dollars ----
+
+g <- gdp_LQ_tbl %>%
+
+    filter(year %in% c(min(year), max(year))) %>%
+
+    ggplot(aes(x = NAICS_desc %>% str_trunc(20, side = c("right")),
+               y = place,
+               text = label_LQ_dollar)) +
+    geom_tile(aes(fill = LQ_dollar)) +
+
+    facet_wrap(~year, scales = "free") +
+
+    scale_fill_gradient2(low = palette_light()[5],
+                         mid = "white",
+                         high = palette_light()[2],
+                         midpoint = 0) +
+    labs(
+        title = "Location Quotient in Dollars",
+        y     = "",
+        x     = "NAICS Sectors - 2 digits"
+    ) +
+
+    theme_tq() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none",
+        plot.title = element_text(face = "bold"),
+        plot.caption = element_text(face = "bold.italic")
+    )
+
+g %>% ggplotly(tooltip = "text")
+
+
+## Line plot ----
+
+## * By location quotient ----
+
+g <- gdp_LQ_tbl %>%
+
+    filter(place == "New Hampshire") %>%
+
+    ggplot(aes(year, LQ,
+               color = NAICS_desc %>% str_trunc(20, side = "right"))) +
+    geom_line() +
+    geom_point(aes(text = label_lq),
+               size = 0.1,
+               alpha = 0.3) +
+
+    theme_tq() +
+    scale_color_tq() +
+
+    labs(
+        title = "New Hampshire's Location Qutient",
+        color = "NAICS Sectors",
+        x     = "",
+        y     = "Location Quotient"
+    )
+
+g %>% ggplotly(tooltip = "text")
+
+
+## * By location quotient in dollars ----
+
+g <- gdp_LQ_tbl %>%
+
+    filter(place == "New Hampshire") %>%
+
+    ggplot(aes(year, LQ_dollar,
+               color = NAICS_desc %>% str_trunc(20, side = "right"))) +
+    geom_line() +
+    geom_point(aes(text = label_LQ_dollar),
+               size = 0.1,
+               alpha = 0.3) +
+
+    theme_tq() +
+    scale_color_tq() +
+    scale_y_continuous(labels = scales::dollar_format(suffix = "B", scale = 1e-3)) +
+
+    labs(
+        title = "New Hampshire's Location Qutient in Dollars",
+        color = "NAICS Sectors",
+        x     = "",
+        y     = "Location Quotient in Dollars"
+    )
+
+g %>% ggplotly(tooltip = "text")
+
+
+# Create a custom function ----
+
+# Two types LQ: LQ in decimal and LQ in dollars
+# LQ_rank_type: 1) LQ_rank 2) LQ_dollar_rank
+# label_type: 1) label_lq 2) label_LQ_dollar
+
+plot_LQ <- function(gdp_LQ_tbl,
+                    is_LQ_dollars = TRUE,
+                    include_all_year = TRUE,
+                    industry_trunc_numb = 20,
+                    panel_spacing_dist = 1,
+                    title_text = "") {
+
+    # Decide which variable is mapped to which axis
+    if (include_all_year) {
+
+        # Decide type of LQ
+        if (is_LQ_dollars) {
+
+            g <- gdp_LQ_tbl %>%
+
+                filter(place %in% c("New Hampshire", "Massachusetts")) %>%
+
+                # Plot
+                ggplot(aes(year %>% as.factor(),
+                           NAICS_desc %>%
+                               str_trunc(industry_trunc_numb, side = "right") %>%
+                               reorder_within(-LQ_dollar_rank, place),
+                           text = label_LQ_dollar)) +
+                geom_tile(aes(fill = -LQ_dollar_rank)) +
+                facet_wrap(~place, scales = "free_y")
+
+        } else {
+
+            g <- gdp_LQ_tbl %>%
+
+                filter(place %in% c("New Hampshire", "Massachusetts")) %>%
+
+                # Plot
+                ggplot(aes(year %>% as.factor(),
+                           NAICS_desc %>%
+                               str_trunc(industry_trunc_numb, side = "right") %>%
+                               reorder_within(-LQ_rank, place),
+                           text = label_lq)) +
+                geom_tile(aes(fill = -LQ_rank)) +
+                facet_wrap(~place, scales = "free_y")
+
+        }
+
+    } else {
+
+        if (is_LQ_dollars) {
+
+            g <- gdp_LQ_tbl %>%
+
+                filter(year %in% c(min(year), max(year))) %>%
+
+                ggplot(aes(x = NAICS_desc %>% str_trunc(industry_trunc_numb, side = c("right")),
+                           y = place,
+                           text = label_LQ_dollar)) +
+                geom_tile(aes(fill = LQ_dollar)) +
+
+                facet_wrap(~year, scales = "free")
+
+        } else {
+
+            g <- g <- gdp_LQ_tbl %>%
+
+                filter(year %in% c(min(year), max(year))) %>%
+
+                ggplot(aes(x = NAICS_desc %>% str_trunc(industry_trunc_numb, side = c("right")),
+                           y = place,
+                           text = label_lq)) +
+                geom_tile(aes(fill = log(LQ))) +
+
+                facet_wrap(~year, scales = "free")
+
+        }
+
+    }
+
+    # Determine value for midpoint
+    if (include_all_year) midpoint_value = -10
+    else midpoint_value = 0
+
+
+    g_formatted <- g  +
+
+        # Formatting
+        scale_fill_gradient2(low = palette_light()[1],
+                             mid = "white",
+                             high = palette_light()[2],
+                             midpoint = midpoint_value) +
+        scale_y_reordered() +
+        theme_tq() +
+        theme(
+            axis.text.x = element_text(angle = 45, hjust = 1),
+            legend.position = "none",
+            plot.title = element_text(face = "bold"),
+            plot.caption = element_text(face = "bold.italic")
+        ) +
+
+        labs(
+            title = title_text,
+            subtitle = "measured in location quotient",
+            caption = "New Hampshire's top industry is Retail while...",
+            x     = "",
+            y     = ""
+        ) +
+
+        theme(panel.spacing=unit(panel_spacing_dist, "cm"))
+
+
+    g_formatted %>% ggplotly(tooltip = "text")
+
+}
+
+gdp_LQ_tbl %>% plot_LQ(is_LQ_dollars = FALSE,
+                       include_all_year = FALSE,
+                       industry_trunc_numb = 20,
+                       panel_spacing_dist = 1,
+                       title_text = "Which industries are major drivers of the economy?")
+
+dump(list = "plot_LQ",
+     file = "00_scripts/plot_LQ.R")
